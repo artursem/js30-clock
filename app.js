7a90ad29a23fafc1b14849093dddf362d0ec1814
artursem <artur.semeniuk@gmail.com>
2021-06-12 18:19:16 +0200

    fix hand animation

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+
 function setDate() {
- const now = new Date();
- const seconds = now.getSeconds();
- const secondsDegree = ((seconds / 60) * 360) + 90;
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegree = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+    console.log(seconds);
+    if (seconds === 0) {
+       secondHand.style.transition = 'all 0s';
+    }
+    if (seconds === 1) {
+       secondHand.style.transition = ' all 0.05s cubic-bezier(0.1, 2.7, 0.58, 1)';
+    }
 
- if (seconds === 59) {
-     
-     console.log('59');
- }
- secondHand.style.transform = `rotate(${secondsDegree}deg)`;
- const minutes = now.getMinutes();
- const minutesDegree = ((minutes / 60) * 360) + 90;
- minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
- const hours = now.getHours();
- const hoursDegree = ((hours / 12) * 360) + 90;
- hourHand.style.transform = `rotate(${hoursDegree}deg)`;
-
-
+    const minutes = now.getMinutes();
+    const minutesDegree = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
+    const hours = now.getHours();
+    const hoursDegree = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegree}deg)`; 
 }
 
 setInterval(setDate, 1000);
